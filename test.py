0ee029fc413de45bf1abb0bb15424fbe0a86c2e3
TaegwanKim <aetaik97@kw.ac.kr>
2024-12-05 20:47:31 +0900

    모델 불러오기 및 예측

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from FinDataLoader import FinDataLoader
 import pickle
+import pandas as pd
 
 DL = FinDataLoader()
 code = '005930'
@@ -10,22 +11,16 @@ data = DL(code,30)
 
 data_len = len(data)
 
-
 train_data = data.iloc[:int(data_len*0.6),1:]
 valid_data = data.iloc[int(data_len*0.6):int(data_len*0.8),1:]
 test_data = data.iloc[int(data_len*0.8):,1:]
 
-print(train_data)
-print(valid_data)
-print(test_data)
+with open(f'{code}_model.pkl','rb') as f:
+    model = pickle.load(f)
+    
+pred = model.predict(test_data.iloc[:,:-1])
 
-rgr = RandomForestRegressor()
-rgr = rgr.fit(train_data.iloc[:,:-1],train_data.iloc[:,-1])
+sr_pred = pd.Series(pred,test_data.index)
 
-with open(f'{code}_model.pkl', 'wb') as f:
-    pickle.dump(rgr, f)
-
-pred = rgr.predict(valid_data.iloc[:,:-1])
-
-print(f"Mean Squared Error : {mean_squared_error(valid_data.iloc[:,-1],pred):.4f}")
-
+print(sr_pred)
+print(f"Mean Squared Error : {mean_squared_error(test_data.iloc[:,-1],pred):.4f}")
